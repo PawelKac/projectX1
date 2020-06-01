c8a5e60c9d8dc8dd5811443e45e6ef894f170bea
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2020-06-01 11:23:24 +0200

    new package

--- src/main/java/Cloth.java
+++ src/main/java/Cloth.java
@@ -1,12 +1,14 @@
+package company;
+
 /**
- * Created by Kaczmarczyk on 2020-04-03.
+ * Created by Kaczmarczyk on 2020-05-22.
  */
 public class Cloth extends Product {
 
     private String size;
     private String material;
 
-    public Cloth(Long id, String productName, Float price, Float weight, String color, int productCount, String size, String material) {
+    public Cloth(Long id, String productName, Float price, Float weight, String color, Integer productCount, String size, String material) {
         super(id, productName, price, weight, color, productCount);
         this.size = size;
         this.material = material;
@@ -22,7 +24,7 @@ public class Cloth extends Product {
 
     @Override
     public String toString() {
-        return super.toString() + "Cloth{" +
+        return super.toString() + "company.Cloth{" +
                 "size='" + size + '\'' +
                 ", material='" + material + '\'' +
                 '}';
