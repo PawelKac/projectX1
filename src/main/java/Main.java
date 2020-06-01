4cbeb2e3f17a325e5f225f58ebb6ce940213c504
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2020-06-01 11:24:03 +0200

    new package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+package company;
+
+/**
+ * Created by Kaczmarczyk on 2020-05-22.
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 }
